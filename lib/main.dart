4e948f74441b1334398d34d0b1d005f05b98c5fa
storage <bgyang1220@gmail.com>
2025-06-19 22:18:10 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,30 @@ import 'package:resume/resume_app.dart';
 import 'package:resume/theme.dart';
 import 'package:provider/provider.dart';
 import 'package:resume/theme_notifier.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
-void main() => runApp(
-  ChangeNotifierProvider(
-    create: (context) => ThemeNotifier(
-      ThemeMode.system == ThemeMode.dark
-        ? Brightness.dark
-        : Brightness.light
-    ),
-    builder: (context, _) {
-      final brightness = Provider.of<ThemeNotifier>(context).themeMode;
-      return MaterialApp(
-        title: 'Resume',
-        debugShowCheckedModeBanner: false,
-        theme: getTheme(brightness: brightness),
-        builder: (context, child) => ScrollConfiguration(
-          behavior: BouncingScrollBehavior(),
-          child: child!
-        ),
-        home: ResumeApp()
-      );
-    }
-  )
-);
+void main() {
+  usePathUrlStrategy();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeNotifier(
+        ThemeMode.system == ThemeMode.dark
+          ? Brightness.dark
+          : Brightness.light
+      ),
+      builder: (context, _) {
+        final brightness = Provider.of<ThemeNotifier>(context).themeMode;
+        return MaterialApp(
+          title: 'Resume',
+          debugShowCheckedModeBanner: false,
+          theme: getTheme(brightness: brightness),
+          builder: (context, child) => ScrollConfiguration(
+            behavior: BouncingScrollBehavior(),
+            child: child!
+          ),
+          home: ResumeApp()
+        );
+      }
+    )
+  );
+}
